69e673238c260b5ced42f3b5387a3112f2fb2c5e
Spharah <mashobane247@gmail.com>
2015-05-15 09:39:16 +0200

    added routing,authentication, test methods

--- server.js
+++ server.js
@@ -14,23 +14,62 @@ app.set('superSecret','p@$$w0rd!');
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
-
-
 app.use(morgan('dev'));
 
-
-app.get('/',function(req,res){
-	res.send('Its working proper.');
-});
-
-
+//enable routting
 var apiRoutes = express.Router();
 
 apiRoutes.get('/values',function(req,res){
-	res.send('Hello from unsecure ser')
+	res.send('Hello from unsecure service')
 }); 
 
 
+//return token
+apiRoutes.get('/authenticate',function(req,res){	
+var user ={};
+
+	var token = jwt.sign(user,app.get('superSecret'),{
+		expiresInMinutes:1440
+	});
+
+	res.json({'token':token})
+});
+
+
+
+//authenticate api 
+apiRoutes.use(function(req,res,next){
+
+	var token = req.body.token || req.query.token || req.headers['x-access-token'];
+	if(token)
+	{
+		jwt.verify(token,app.get('superSecret'),function(err,decoded){
+			if(err){
+				return res.json({success:true,message:'Failed to authenticate'});
+			}
+			else
+			{
+				req.decoded=decoded;
+				next();
+			}
+		});
+	}
+	else
+	{
+		return res.status(403).send({
+			success:false,
+			message:'No token provided'
+		});
+	}
+});
+
+
+apiRoutes.get('/values/1',function(req,res){
+	res.send('Hello from secure service')
+}); 
+
+
+app.use('/api',apiRoutes);
 app.listen(port);
 console.log('Server started at http://localhost:'+port);
 
